29d2490b91d2c7902528aedb5ba89f8d88df96b6
Nathan Maciel <112430114+nathanmacielviana@users.noreply.github.com>
2023-10-15 19:10:01 -0300

    Add files via upload

--- HelpSaude-main/polls/views.py
+++ HelpSaude-main/polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Question
 def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    latest_question_list = Question.objects.order_by('public_date')
     template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list':latest_question_list,
